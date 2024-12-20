91f3305e6d2153849bbc586860da475a8fd32fb6
Gustavo Henrique Teixeira Poeta <gustavpoeta@gmail.com>
2024-12-20 09:27:17 -0300

    feat: add to-do item model

---
+++ API/Models/Item.cs
@@ -0,0 +1,59 @@
+﻿namespace API.Models
+{
+    public class Item
+    {
+
+        private int _id = 0;
+        private string _name = string.Empty;
+        private string _description = string.Empty;
+
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public string Description
+        {
+            get => _description;
+            set => _description = value;
+        }
+
+        public Item(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+
+
+    }
+
+    public class ItemFactory
+    {
+        public Item Create(int id, string name, string description)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentException("Id is not valid.");
+            }
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Name is not valid");
+            }
+
+            return new Item(
+                id,
+                name, 
+                string.IsNullOrWhiteSpace(description) ? string.Empty : description
+                );
+        }
+    }
+}
