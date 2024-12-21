26cf89dbb7f611ef48837e657cd65d3c5a3ce438
Gustavo Henrique Teixeira Poeta <gustavpoeta@gmail.com>
2024-12-21 14:48:22 -0300

    feat: add vite w/ vue template, and add item component

---
+++ todo/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">    
+  import item from "./components/item.vue";
+</script>
+
+<template>
+  <main class="w-screen h-screen bg-indigo-100 p-12">
+    <item name="tarefa de casa"/>
+  </main>
+</template>
---
+++ todo/src/components/item.vue
@@ -0,0 +1,16 @@
+<script>
+    export default {
+        props: {
+            name: {
+                type: String,
+                required: true,
+            },
+        },
+    };
+</script>
+
+<template>
+    <div class="w-80 h-24 bg-indigo-300 text-indigo-900 font-mono text-lg rounded-2xl flex items-center justify-center">
+        <h2>{{ name }}</h2>
+    </div>
+</template>
